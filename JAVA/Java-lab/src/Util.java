b968f5e2a694e4cafc0b800fb78192aba5b64f12
Ahyun Kim <zlzzlzz2l@naver.com>
2021-12-19 00:58:07 +0900

    Delete: Fixed untracked files

---
+++ JAVA/Java-lab/src/Util.java
@@ -0,0 +1,13 @@
+public class Util {
+    public static <K, V> boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
+        boolean keyCompare = p1.getKey().equals(p2.getKey());
+        boolean valueCompare = p1.getValue().equals(p2.getValue());
+        return keyCompare && valueCompare;
+    }
+
+    public static <T extends Number> int compare(T t1, T t2) {
+        double v1 = t1.doubleValue();
+        double v2 = t2.doubleValue();
+        return Double.compare(v1, v2);
+    }
+}
